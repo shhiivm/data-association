f92074d0a008c1965d74407ad959fd03c1a2bd78
shhiivm <shhiivm@pm.me>
2025-09-27 19:04:08 +0530

    created post schema, pass encrypted, attached cookie with signin token

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const app = express();
+const bcrypt = require('bcrypt');
 
-const userModel = require('./model/user');
+const userModel = require("./model/user");
+const postModel = require("./model/post")
+
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+app.post("/create", (req, res) => {});
 
-app.post("/create", (req,res) =>{
-
+app.post('/register', async (req,res) =>{
+  let {email,password, username, name, age} = req.body;
+  bcrypt.genSalt(10, async (err, hash) =>{
+    let user = await userModel.create({
+      username,
+      email,
+      age,
+      name,
+      password: hash,
+    });
+    let token = jwt.sign({email:email, userid: user._id}, "shhhhh");
+    res.cookie("token", token);
+    res.send("registered");
+  })
 })
 
 app.listen(8000);
--- model/user.js
+++ model/user.js
@@ -10,6 +10,7 @@ const userSchema = mongoose.Schema({
   age: Number,
   email: String,
   password: String,
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
 });
 
 module.exports = mongoose.model("user", userSchema);
