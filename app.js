fbb84a75a5166e8a2f5d1ea0cae1e8049ce78401
shhiivm <shhiivm@pm.me>
2025-09-29 22:22:33 +0530

    added isLoggedIn feature

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const userModel = require("./model/user");
-const postModel = require("./model/post")
+const postModel = require("./model/post");
 
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
@@ -17,22 +17,67 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/create", (req, res) => {});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.post('/register', async (req,res) =>{
-  let {email,password, username, name, age} = req.body;
-  bcrypt.genSalt(10, async (err, hash) =>{
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("login");
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    let { email, password } = req.body;
+
+    let user = await userModel.findOne({ email });
+    if (!user) return res.status(401).send("Invalid email or password");
+
+    let isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).send("Invalid email or password");
+
+    let token = jwt.sign({ email: email, userid: user._id }, "shhhhh");
+    res.cookie("token", token);
+    res.status(200).send("You are logged in");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    let { email, password, username, name, age } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     let user = await userModel.create({
       username,
       email,
       age,
       name,
-      password: hash,
+      password: hashedPassword,
     });
-    let token = jwt.sign({email:email, userid: user._id}, "shhhhh");
+
+    let token = jwt.sign({ email: email, userid: user._id }, "shhhhh");
     res.cookie("token", token);
     res.send("registered");
-  })
-})
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
+
+app.get("/profile", isLoggedIn, (req, res) => {
+  res.send(req.user);
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.cookies.token === "") res.send("You must be logged in");
+  else {
+    let data = jwt.verify(req.cookies.token, "shhhhh");
+    req.user = data;
+    next();
+  }
+}
 
 app.listen(8000);
